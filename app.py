740032f6395607eb7a7af837e06d1d47b771dc11
Pavani Vaishnavi <pavanivaishnavi004@gmail.com>
2025-07-06 21:44:42 +0530

    modi

--- app.py
+++ app.py
@@ -217,5 +217,5 @@ def admin_logout():
 
 # ---------- Run Server ---------- #
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))  # Render provides PORT env variable
     app.run(debug=False, host='0.0.0.0', port=port)
